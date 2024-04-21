89c062586d80ab152c69140f3f8eedf37aa8ebc9
Jazz <jcronin@unca.edu>
2024-04-21 18:00:38 -0400

    added the second part of creating the back end

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-
-@app.get("/")
+@app.get("/api/v1/hello")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/")
+async def get_index():
+    with open("index.html") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content, status_code=200)
---
+++ check.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+poetry run black .
+poetry run isort .
+poetry run flake8 .
